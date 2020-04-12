6aea0725cf4f74fddede093b374bec4cb29ff452
mariusud <mariusudnaes@gmail.com>
2020-04-12 22:21:22 +0200

    finished with problem 1

--- README.md
+++ README.md
@@ -5,3 +5,21 @@ Classification of Flowers and handwritten numbers, for the course TT4275
 ## Part 1 - Iris recognizition
 For this task, a linear classifier was trained on 30 samples and tested on 20 with focus on design and testing.
 Confusion matrix and error rate was found for both train/test sets to compare performance.
+
+### With 30 samples training and 20 for testing we get
+correct guesses:  96.66666666666667 %
+Minimum mean-squared error obtained:  0.062082223491377604
+confusion matrix:
+ [[20.  0.  0.  0.]
+ [ 0. 18.  0.  0.]
+ [ 0.  2. 20.  0.]
+ [ 0.  0.  0.  0.]]
+
+### With 30 samples training and 20 for testing we get
+correct guesses:  95.0 %
+Minimum mean-squared error obtained:  0.05412946623538162
+confusion matrix:
+ [[20.  1.  0.  0.]
+ [ 0. 19.  2.  0.]
+ [ 0.  0. 18.  0.]
+ [ 0.  0.  0.  0.]]
--- iris.py
+++ iris.py
@@ -11,31 +11,38 @@ classmap = {
     'Iris-virginica': np.array([0, 0, 1])
 }
 
-def get_data(cutting_position):
-    '''cutting_position: where to cut the training and test set e.g. 30/20'''
-
-    x, t = [], []
-    f = open('./iris_dataset/iris.data')
-    line = f.readline()
-    while line:
-        line = line.strip('\n')
-        if line == '':  
-            continue
-        line = line.split(',')
-        x.append(np.array([float(i) for i in line[:-1]]))
-        t.append(classmap.get(line[-1]))
-        line = f.readline()
-    f.close()
-
+def arrange_order(x,t):
+    '''
+    Sets up the array such that the first 90 rows are 30*3 sets of different flowers
+    '''
     array_x, array_t = [], []
-
     for i in range(50):
         for j in range(3):
             array_x.append(x[50*j+i])
             array_t.append(t[50*j+i])
-
     return np.array(array_x), np.array(array_t)
 
+def get_x():
+    f = open('./iris_dataset/iris.data')
+    x = np.loadtxt(f,delimiter=',',usecols=(0,1,2,3))
+    f.close()
+    return x
+
+def get_t():
+    f = open('./iris_dataset/iris.data')
+    t = np.loadtxt(f,delimiter=',', usecols=(4),dtype=str)
+    f.close()
+    t_updated = []
+    for line in np.nditer(t):   
+        t_updated.append(classmap.get(str(line)))
+    return t_updated
+
+def get_data(edit_order=True):
+    x = get_x()
+    t = get_t()
+    if edit_order:
+        x,t = arrange_order(x,t)
+    return x,t
 
 def MSE(A,B):
     '''
@@ -45,6 +52,9 @@ def MSE(A,B):
     return mse
 
 def gradient_MSE(x, g, t):
+    '''
+    Calculates the gradient of the Mean-Squared Error
+    '''
     mse_gradient = g - t
     g_gradient = g * (1-g)
     zk_gradient = x.T
@@ -53,8 +63,7 @@ def gradient_MSE(x, g, t):
 def sigmoid(x):
     '''returns the sigmoid, an acceptable approximation of the heaviside function'''
     return (1/(1+np.exp(-x)))
-
-    
+  
 def train(x, t, alpha, iterations):
     '''
     x - training data,
@@ -63,30 +72,55 @@ def train(x, t, alpha, iterations):
     W - weight matrix,
     g - output vector,
     '''
-    W = np.random.normal(0, 1, (3, x.shape[1]))
+    W = np.zeros((3,4))
     mse_values = []
     for i in range(iterations):
         g = sigmoid(np.dot(x,W.T))
         W = W - alpha * gradient_MSE(x, g, t).T
-        mse_values.append(MSE(g,t))
+        mse_values.append(MSE(g,t).mean())
     return W, mse_values 
-
 
 def predict(W,x):
     g = np.dot(x, W.T)
-    return g
+    return np.argmax(sigmoid(g), axis=1)
 
 
-
+def confusion_matrix(predictions, actual_class, features):
+    '''
+    Creates a confusion matrix for the classifier.
+    predictions - predicted class.
+    actual_class - what class the row is classes.
+    features - the number of features predicted.
+    '''
+    confusion = np.zeros((features, features))
+    for i in range(len(predictions)):
+        confusion[actual_class[i]][predictions[i]] += 1
+    return confusion.T
 
 if __name__ == '__main__':
 
-    iterations = 100
-    alpha = 0.2
+    iterations = 100000
+    alpha = 0.01
+    x,t = get_data()
 
-    x,t = get_data(30)
     W, mse_values = train(x[:90], t[:90], alpha, iterations)
-
     predictions = predict(W,x[90:])
-    print(predictions)
+    trueclass = np.argmax(t[90:], axis=1)
+
+    confusion = confusion_matrix(predictions,trueclass,4)
+    print("With 30 samples training and 20 for testing we get")
+    print("correct guesses: ",100*np.sum(predictions == trueclass) / predictions.size, "%")
+    print("Minimum mean-squared error obtained: ",mse_values[len(mse_values) -1])
+    print("confusion matrix:\n",confusion)
+
+    W, mse_values = train(x[60:], t[60:], alpha, iterations)
+    predictions = predict(W,x[:60])
+    trueclass = np.argmax(t[:60], axis=1)
+
+    confusion = confusion_matrix(predictions,trueclass,4)
+    print("With 20 samples training and 30 for testing we get")
+    print("correct guesses: ",100*np.sum(predictions == trueclass) / predictions.size, "%")
+    print("Minimum mean-squared error obtained: ",mse_values[len(mse_values) -1])
+    print("confusion matrix:\n",confusion)
+
 
