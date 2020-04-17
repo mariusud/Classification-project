8f9fc7e4038199818b057f4afc9196344b5e960c
mariusud <mariusudnaes@gmail.com>
2020-04-17 15:59:15 +0200

    added gaussian mixture model

--- wovels.py
+++ wovels.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import numpy as np
 from scipy.stats import multivariate_normal
+from sklearn.mixture import GaussianMixture
 
 def get_data():
     '''
@@ -48,11 +49,12 @@ class Gaussian_model():
     'uw' : "who'd"
     }
 
-    def __init__(self,data,classname):
+    def __init__(self,data,classname,diagonal=False):
         '''
         Initializes the class and splits data into train/test set
         '''
         self.x_train, self.y_train, self.x_test, self.y_test = self.split(data, classname)
+        self.diagonal = diagonal
 
     def split(self, data, classname):
         '''
@@ -76,7 +78,8 @@ class Gaussian_model():
             vowel_samples = self.x_train[vowel_indeces]
             sample_mean = sample_mean_vector(vowel_samples)
             sample_covariance = sample_covariance_matrix(sample_mean,vowel_samples)
-
+            if self.diagonal:
+                sample_covariance = np.diag(np.diag(sample_covariance))
             self.rv = multivariate_normal(mean=sample_mean,cov=sample_covariance)
             probability_distribution[i] = self.rv.pdf(self.x_train)
 
@@ -96,7 +99,8 @@ class Gaussian_model():
             vowel_samples = self.x_test[vowel_indeces]
             sample_mean = sample_mean_vector(vowel_samples)
             sample_covariance = sample_covariance_matrix(sample_mean,vowel_samples)
-
+            if self.diagonal:
+                sample_covariance = np.diag(np.diag(sample_covariance))
             self.rv = multivariate_normal(mean=sample_mean,cov=sample_covariance)
             probability_distribution[i] = self.rv.pdf(self.x_test)
 
@@ -118,8 +122,93 @@ class Gaussian_model():
             confusion[actual_class[i]][predictions[i]] += 1
         return confusion.T     
 
-if __name__ == '__main__':
-    data, classname = get_data()
+
+class GaussianMixture_model():
+    vowels= {'ae' : "had",
+    'ah' : "hod",
+    'aw' : "hawed",
+    'eh' : "head",
+    'er' : "heard",
+    'ei' : "haid",
+    'ih' : "hid",
+    'iy' : "heed",
+    'oa' : "/o/ as in boat",
+    'oo' : "hood",
+    'uh' : "hud", 
+    'uw' : "who'd"
+    }
+
+    def __init__(self, data, classname,components, diagonal=False):
+        '''
+        Initializes the class and splits data into train/test set
+        '''
+        self.x_train, self.y_train, self.x_test, self.y_test = self.split(data, classname)
+        self.diagonal = diagonal
+        self.components = components
+        self.GMMs =  dict.fromkeys(range(len(self.vowels)))
+
+
+    def split(self, data, classname):
+        '''
+        Splits the data set into train/test set.
+        '''
+        train, test = [], []
+        for vowel in self.vowels:
+            vowel_arr = find_indeces(vowel, classname)
+            train.extend(vowel_arr[:70])
+            test.extend(vowel_arr[70:])
+        return data[train], classname[train], data[test], classname[test]
+
+    def train(self):
+        '''
+        Model training.
+        Model based on sklearn's Gaussian Mixture
+        '''
+        probability_distribution = np.zeros((12,self.x_train.shape[0]))
+        for i, vowel in enumerate(self.vowels):
+            vowel_indeces = find_indeces(vowel,self.y_train) 
+            vowel_samples = self.x_train[vowel_indeces]
+            sample_mean = sample_mean_vector(vowel_samples)
+            sample_covariance = sample_covariance_matrix(sample_mean,vowel_samples)
+            if self.diagonal:
+                sample_covariance = np.diag(np.diag(sample_covariance))
+            self.GMMs[i] = GaussianMixture(n_components=self.components, covariance_type='diag',max_iter=500000)
+            self.GMMs[i].fit(vowel_samples, self.y_train[vowel_indeces])
+            probability_distribution[i] = self.GMMs[i].score_samples(self.x_train)
+
+        self.predicted_train = np.argmax(probability_distribution, axis=0)
+        self.true_train = np.asarray([i for i in range(12) for _ in range(70)])
+
+    def train_accuracy(self):
+        print("Model accuracy for training set: ",np.sum(self.predicted_train==self.true_train)/len(self.predicted_train))
+
+    def predict(self):
+        '''
+        Predicts vowel class based on the gaussian class model
+        '''
+        probability_distribution = np.zeros((12,self.x_test.shape[0]))
+        for i, vowel in enumerate(self.vowels):
+            probability_distribution[i] = self.GMMs[i].score_samples(self.x_test)
+        
+        self.predicted_test = np.argmax(probability_distribution, axis=0)
+        self.true_test = np.asarray([i for i in range(12) for _ in range(69)])
+
+    def prediction_accuracy(self):
+        print("Model accuracy for testing set: ",np.sum(self.predicted_test==self.true_test)/len(self.predicted_test))
+
+    def confusion_matrix(self, predictions, actual_class, features):
+        '''
+        Creates a confusion matrix for the classifier.
+        predictions - predicted class.
+        actual_class - what class the row is classes.
+        features - the number of features predicted.
+        '''
+        confusion = np.zeros((features, features))
+        for i in range(len(predictions)):
+            confusion[actual_class[i]][predictions[i]] += 1
+        return confusion.T     
+
+def problem_1(data,classname):
     model = Gaussian_model(data,classname)
     model.train()
     model.predict()
@@ -132,3 +221,28 @@ if __name__ == '__main__':
 
     model.train_accuracy()
     model.prediction_accuracy()
+
+    print("\ndiagonal model\n")
+    diagonal_model = Gaussian_model(data,classname,True)
+    diagonal_model.train()
+    diagonal_model.predict()
+
+    diag_training_confusion = diagonal_model.confusion_matrix(diagonal_model.predicted_train,diagonal_model.true_train,12)
+    diag_testing_confusion = diagonal_model.confusion_matrix(diagonal_model.predicted_test,diagonal_model.true_test,12)
+    print(diag_training_confusion)
+    print(diag_testing_confusion)
+    diagonal_model.train_accuracy()
+    diagonal_model.prediction_accuracy()
+
+def problem_2(data,classname):
+    gmm = GaussianMixture_model(data,classname,3)
+    gmm.train()
+    gmm.predict()
+    gmm.train_accuracy()
+    gmm.prediction_accuracy()
+    training_confusion = gmm.confusion_matrix(gmm.predicted_train,gmm.true_train,12)
+    print(training_confusion)
+
+if __name__ == '__main__':
+    data, classname = get_data()
+    problem_2(data,classname)
